0b6fbb3e89795a67df7eda8919e9b5052a77bd2c
jcsilvaaa <162688556+jcsilvaaa@users.noreply.github.com>
2025-10-18 17:27:40 +0800

    SplashActivity.java

--- app/src/main/java/com/mobdeve/s17/MC02/SplashActivity.java
+++ app/src/main/java/com/mobdeve/s17/MC02/SplashActivity.java
@@ -8,8 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static final int SPLASH_DURATION = 2000; // 2 seconds
-
+    private static final int SPLASH_DURATION = 2000; 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
